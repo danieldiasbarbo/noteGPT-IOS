0320910888187ee55eff17255c9d9fa4e61f26a6
Student05 <student05@HackaTruck05.local>
2023-08-21 19:20:30 -0300

    first commit

---
+++ noteGPT/noteGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  noteGPTApp.swift
+//  noteGPT
+//
+//  Created by Student05 on 21/08/23.
+//
+
+import SwiftUI
+
+@main
+struct noteGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
