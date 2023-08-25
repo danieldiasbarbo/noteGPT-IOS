87f895185365be0d66266945c16b2a63febb8826
Student <student@HackaTruck20.local>
2023-08-24 21:15:29 -0300

    ai popup fix

--- noteGPT/Notes_view/note_view.swift
+++ noteGPT/Notes_view/note_view.swift
@@ -12,37 +12,42 @@ struct note_view: View {
     var nota = "Mussum Ipsum, cacilds vidis litro abertis. Aenean aliquam molestie leo, vitae iaculis nisl. Mauris nec dolor in eros commodo tempor. Aenean aliquam molestie leo, vitae iaculis nisl. Praesent malesuada urna nisi, quis volutpat erat hendrerit non. Nam vulputate dapibus. Detraxit consequat et quo num tendi nada."
     var path = "caminho"
     var head = "Projeto"
-    @State var AI_popup = true
+    @State var AI_popup = false
+    
+    func showPopup() {
+            AI_popup.toggle()
+        }
     
     var body: some View {
-        VStack(spacing: 0){
-            header(title: head)
-            app_path(path: path)
-            Spacer()
-            ZStack(alignment: .center){
-                Rectangle()
-                    .frame(width: 350, height: 500)
-                    .cornerRadius(30)
-                    .foregroundColor(.white)
-                Text(nota).frame(width: 300, height: 450)
-                VStack{
-                    HStack{
-                        Button(){
-                            AI_popup.toggle()
-                        } label: {
-                            Image(systemName: "brain.head.profile")
-                                .resizable()
-                                .frame(width: 20, height: 25)
-                                .padding(40)
-                                .foregroundColor(Color("azul_claro"))
+            VStack(spacing: 0){
+                header(title: head)
+                app_path(path: path)
+                Spacer()
+                ZStack(alignment: .center){
+                    Rectangle()
+                        .frame(width: 350, height: 500)
+                        .cornerRadius(30)
+                        .foregroundColor(.white)
+                    Text(nota).frame(width: 300, height: 450)
+                    VStack{
+                        HStack{
+                            Button(action: showPopup) {
+                                Image(systemName: "brain.head.profile")
+                                    .resizable()
+                                    .frame(width: 20, height: 25)
+                                    .padding(40)
+                                    .foregroundColor(Color("azul_claro"))
+                            }
                         }
+                        Spacer()
                     }
-                    Spacer()
                 }
+                Spacer()
+            }.background(Color("azul_escuro")).ignoresSafeArea()
+            .sheet(isPresented: $AI_popup) {
+                PopupView(isPresented: $AI_popup)
             }
-            Spacer()
-        }.background(Color("azul_escuro")).ignoresSafeArea()
-    }
+        }
     
     init() {
         
